ca111dda0d3fb2564e25e982dced801b1bbeb378
henry chang <a68727739@gmail.com>
2018-02-25 17:43:22 +0800

    first

---
+++ defuseBomb/defuseBomb/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace defuseBomb
+{
+    
+	public partial class MainPage : ContentPage
+	{
+        static string bomb = new Random().Next(1, 4).ToString();
+        static int scores = 0;
+        public MainPage()
+		{
+			InitializeComponent();
+		}
+        async void ButtonClicked(object sender,EventArgs e)
+        {
+            Button button = sender as Button;
+            //Game Over
+            if (button.Text == bomb)
+            {
+                await DisplayAlert("Bomb Exploded","GAME OVER","Retry");
+                bomb = new Random().Next(1, 4).ToString();
+                scores = 0;
+            }
+            else
+            {
+                scores += 1;
+                await DisplayAlert("Bomb defused", "Scores:" + scores, "continue");
+            }
+        }
+	}
+}
